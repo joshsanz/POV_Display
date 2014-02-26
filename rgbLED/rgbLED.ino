d7d190cec4bec36398e6a21943e7f87ede2088ca
Josh Sanz <joshsanz@gmail.com>
2014-02-26 09:33:12 -0800

    fixed merge errors in arduino code
    not sure why merge was so screwy

--- rgbLED/rgbLED.ino
+++ rgbLED/rgbLED.ino
@@ -144,94 +144,6 @@ void heart1(int i, int j) {
   }
 }
 
-<<<<<<< HEAD
-<<<<<<< HEAD
-void setAddress(int i) {
-if(i==0) {
-digitalWrite(A,0);
-digitalWrite(B,0);
-digitalWrite(C,0);
-digitalWrite(D,0);
-} else if(i==1) {
-digitalWrite(A,1);
-digitalWrite(B,0);
-digitalWrite(C,0);
-digitalWrite(D,0);
-}else if(i==2) {
-digitalWrite(A,0);
-digitalWrite(B,1);
-digitalWrite(C,0);
-digitalWrite(D,0);
-}else if(i==3) {
-digitalWrite(A,1);
-digitalWrite(B,1);
-digitalWrite(C,0);
-digitalWrite(D,0);
-}else if(i==4) {
-digitalWrite(A,0);
-digitalWrite(B,0);
-digitalWrite(C,1);
-digitalWrite(D,0);
-}else if(i==5) {
-  //Serial.println("i=5");
-digitalWrite(A,1);
-digitalWrite(B,0);
-digitalWrite(C,1);
-digitalWrite(D,0);
-}else if(i==6) {
-digitalWrite(A,0);
-digitalWrite(B,1);
-digitalWrite(C,1);
-digitalWrite(D,0);
-}else if(i==7) {
-digitalWrite(A,1);
-digitalWrite(B,1);
-digitalWrite(C,1);
-digitalWrite(D,0);
-}else if(i==8) {
-digitalWrite(A,0);
-digitalWrite(B,0);
-digitalWrite(C,0);
-digitalWrite(D,1);
-}else if(i==9) {
-digitalWrite(A,1);
-digitalWrite(B,0);
-digitalWrite(C,0);
-digitalWrite(D,1);
-}else if(i==10) {
-digitalWrite(A,0);
-digitalWrite(B,1);
-digitalWrite(C,0);
-digitalWrite(D,1);
-}else if(i==11) {
-  //Serial.println("i=11");
-digitalWrite(A,1);
-digitalWrite(B,1);
-digitalWrite(C,0);
-digitalWrite(D,1);
-}else if(i==12) {
-digitalWrite(A,0);
-digitalWrite(B,0);
-digitalWrite(C,1);
-digitalWrite(D,1);
-}else if(i==13) {
-digitalWrite(A,1);
-digitalWrite(B,0);
-digitalWrite(C,1);
-digitalWrite(D,1);
-}else if(i==14) {
-digitalWrite(A,0);
-digitalWrite(B,1);
-digitalWrite(C,1);
-digitalWrite(D,1);
-}else{
-digitalWrite(A,1);
-digitalWrite(B,1);
-digitalWrite(C,1);
-digitalWrite(D,1);
-=======
-=======
->>>>>>> origin/Hearts-Display
 /* logic for medium heart */
 void heart2(int i, int j) {
   if (i == 10 && j > 5 && j < 13) {
@@ -270,11 +182,6 @@ void heart2(int i, int j) {
     digitalWrite(r1, 0);
     digitalWrite(r2, 0);
   }
-<<<<<<< HEAD
->>>>>>> origin/Hearts-Display
-=======
->>>>>>> origin/Hearts-Display
-}
 
 /* logic for largest heart */
 void heart3(int i, int j) {
@@ -346,48 +253,6 @@ void setup() {
 /* main loop which drives the display and reads in light values */
 void loop() {
   int j;
-<<<<<<< HEAD
-<<<<<<< HEAD
-  for(j=0;j<32;j++) {
-	digitalWrite(r1,0);
-	digitalWrite(r2,0);
-	digitalWrite(g1,1);
-	digitalWrite(g2,1);
-	digitalWrite(b1,0);
-	digitalWrite(b2,0);
-	//delayMicroseconds(10);
-	if(i==10 && j==10) {
-                //Serial.println("white");
-		digitalWrite(r1,1);
-		digitalWrite(r2,1);
-		digitalWrite(g1,0);
-		digitalWrite(g2,0);
-		digitalWrite(b1,0);
-		digitalWrite(b2,0);
-		//delayMicroseconds(1);
-	}
-	digitalWrite(CLK,1);
-	//delayMicroseconds(10);
-	digitalWrite(CLK,0);
-}
-digitalWrite(OE,1);
-//delayMicroseconds(10);
-digitalWrite(LAT,1);
-//delayMicroseconds(10);
-digitalWrite(LAT,0);
-//digitalWrite(A,1);
-//digitalWrite(B,0);
-//digitalWrite(C,1);
-//digitalWrite(D,0);
-setAddress(i);
-//delayMicroseconds(10);
-digitalWrite(OE,0);
-++i;
-if(i==16) i=0;
-//delay(500);
-=======
-=======
->>>>>>> origin/Hearts-Display
   int light;
   // read in value from photo resistor/voltage divider circuit
   light = analogRead(8);
@@ -441,10 +306,6 @@ if(i==16) i=0;
   // increment row, loop ends at 15
   ++i;
   if (i == 16) i = 0;
-<<<<<<< HEAD
->>>>>>> origin/Hearts-Display
-=======
->>>>>>> origin/Hearts-Display
 }
 
 /* 	We tried implementing lookup tables for the display logic, but they were too slow
@@ -558,8 +419,5 @@ const int Heart3[] = {
 0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,
 0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0
 };
-<<<<<<< HEAD
 */
-=======
-*/
->>>>>>> origin/Hearts-Display
+
